0d876fb4710060df0bbf21262035d6a41f7d66c9
Lee <leeirungu11313@gmail.com>
2024-06-05 16:40:11 +0300

    Python

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+  ('Mr. Chocolate',20, 'M', 'brown', 'friendly', 1),
+  ('Rowdy', 2, 'M', 'black', 'playful', 1),
+  ('Tabitha', 2, 'F', 'white', 'curious', 1),
+  ('Sergeant Brown', 10, 'M', 'brown', 'erious', 1),
+  ('Melissa', 4, 'F', 'gray', 'gentle', 1),
+  ('Grinch', 2, 'M', 'green', 'grumpy', 1),
+  ('Wendy', 1, 'F', 'yellow', 'playful', 1),
+  (NULL, 7, 'M', 'blue', 'lazy', 1);
